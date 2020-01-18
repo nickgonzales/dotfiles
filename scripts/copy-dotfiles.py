a2435d3a75674215224979ef7f0df084babdd25d
Nick Gonzales <nickjamesgonzales@gmail.com>
2020-01-18 10:49:54 -0700

    Add script to replace dotfiles

---
+++ scripts/copy-dotfiles.py
@@ -0,0 +1,22 @@
+import os
+import os.path as path
+
+repo_dir = path.realpath(path.join(__file__, '../..'))
+home_dir = os.environ['HOME']
+
+home_src = path.join(repo_dir, 'home')
+
+for root, dirs, files in os.walk(home_src):
+    rel_file_dir = path.relpath(root, home_src)
+    os.makedirs(path.join(home_dir, rel_file_dir), exist_ok=True)
+
+    for file in files:
+        src_file_path = path.join(root, file)
+        dst_file_path = path.join(home_dir, rel_file_dir, file)
+
+        if path.islink(dst_file_path):
+            os.unlink(dst_file_path)
+        elif path.isfile(dst_file_path):
+            os.remove(dst_file_path)
+        
+        os.symlink(src_file_path, dst_file_path)
